4fbc45293eeead2b2b24f7accd245af7f3c4b1b6
Oleksandr.Gonchar <oleksandr_gonchar@epam.com>
2017-02-27 17:40:46 +0300

    add two components history and calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HistoryComponent } from '../components/history/history.component';
+import { CalculatorComponent } from '../components/calculator/calculator.component';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    HistoryComponent,
+    CalculatorComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp)
